31c1a489ad6d6b5c45ce9eb849aaa182cd0693f7
Gayatri Patil <patilgayatri495@gmail.com>
2026-09-22 19:57:25 +0530

    Update Variables.java

---
+++ java_basics/Variables.java
@@ -0,0 +1,23 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        // Declaring and initializing variables
+        String name = "Gayatri";
+        int age = 21;
+        double percentage = 83.5;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        // Printing variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Percentage: " + percentage);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+
+        // Changing variable value
+        age = 22;
+
+        System.out.println("Updated Age: " + age);
+    }
+}
