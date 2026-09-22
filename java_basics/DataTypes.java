2a9c6c58cdf6198f4b204c510146c5ec4c2b7929
Gayatri Patil <patilgayatri495@gmail.com>
2026-09-22 19:56:30 +0530

    Update DataTypes.java

---
+++ java_basics/DataTypes.java
@@ -0,0 +1,33 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        // Integer types
+        byte b = 100;
+        short s = 20000;
+        int i = 500000;
+        long l = 10000000000L;
+
+        // Decimal types
+        float f = 10.5f;
+        double d = 99.99;
+
+        // Character
+        char c = 'G';
+
+        // Boolean
+        boolean isJavaEasy = true;
+
+        // String (Non-primitive)
+        String language = "Java";
+
+        System.out.println("Byte: " + b);
+        System.out.println("Short: " + s);
+        System.out.println("Integer: " + i);
+        System.out.println("Long: " + l);
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+        System.out.println("Character: " + c);
+        System.out.println("Boolean: " + isJavaEasy);
+        System.out.println("String: " + language);
+    }
+}
