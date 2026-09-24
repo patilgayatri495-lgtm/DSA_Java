ad2189b3d21a832522a5d24f73d0f6098571f8fa
Gayatri Patil <patilgayatri495@gmail.com>
2026-09-24 11:01:18 +0530

    Create conditional.java

---
+++ java_basics/conditional.java
@@ -0,0 +1,269 @@
+// 1. IF STATEMENT
+public class Main {
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        if (age >= 18) {
+            System.out.println("You are an adult.");
+        }
+    }
+}
+// 2. IF-ELSE STATEMENT
+public class Main {
+    public static void main(String[] args) {
+
+        int number = 10;
+
+        if (number % 2 == 0) {
+            System.out.println("Even number");
+        } else {
+            System.out.println("Odd number");
+        }
+    }
+}
+// 3. IF-ELSE-IF STATEMENT
+public class Main {
+    public static void main(String[] args) {
+
+        int marks = 75;
+
+        if (marks >= 90) {
+            System.out.println("Grade A+");
+        } else if (marks >= 75) {
+            System.out.println("Grade A");
+        } else if (marks >= 60) {
+            System.out.println("Grade B");
+        } else if (marks >= 40) {
+            System.out.println("Grade C");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
+// 4. NESTED IF STATEMENT
+public class Main {
+    public static void main(String[] args) {
+
+        int age = 20;
+        boolean hasLicense = true;
+
+        if (age >= 18) {
+            if (hasLicense) {
+                System.out.println("You can drive.");
+            }
+        }
+    }
+}
+// 5. SWITCH STATEMENT
+public class Main {
+    public static void main(String[] args) {
+
+        int day = 2;
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            default:
+                System.out.println("Invalid day");
+        }
+    }
+}
+// 5. SWITCH STATEMENT
+public class Main {
+    public static void main(String[] args) {
+
+        int day = 2;
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            default:
+                System.out.println("Invalid day");
+        }
+    }
+}
+v// 5. SWITCH STATEMENT
+public class Main {
+    public static void main(String[] args) {
+
+        int day = 2;
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            default:
+                System.out.println("Invalid day");
+        }
+    }
+}
+// 5. SWITCH STATEMENT
+public class Main {
+    public static void main(String[] args) {
+
+        int day = 2;
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            default:
+                System.out.println("Invalid day");
+        }
+    }
+}
+// 5. SWITCH STATEMENT
+public class Main {
+    public static void main(String[] args) {
+
+        int day = 2;
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            default:
+                System.out.println("Invalid day");
+        }
+    }
+}
+// 5. SWITCH STATEMENT
+public class Main {
+    public static void main(String[] args) {
+
+        int day = 2;
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            default:
+                System.out.println("Invalid day");
+        }
+    }
+}
+// 5. SWITCH STATEMENT
+public class Main {
+    public static void main(String[] args) {
+
+        int day = 2;
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            default:
+                System.out.println("Invalid day");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
